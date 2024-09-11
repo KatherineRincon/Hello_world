3bc82ff888af507b7a04c6344ac9cf24d89d3b32
KatherineRincon <ingry_krincon@soy.sena.edu.co>
2024-09-11 16:44:53 -0500

    se agrego un archivo prueba2.py

--- Prueba.py
+++ Prueba.py
@@ -1,4 +1,10 @@
 import time
+print ("Hola") # esta linea emprime en consola la palabra hola
 print ("Hello World \n")
 time.sleep(3)
-print ("segundo reglon")
+#print ("segundo reglon")
+
+print(type("hola")) #la funcion type me dice que tipo de dato esta en parentesis
+print(type(4))
+print(type(5.5))
+      
